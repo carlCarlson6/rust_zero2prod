bb7ac85b5dcdc10c30757d3d417b07b41aa14c44
carl <carlos.acitores@colba.es>
2024-04-05 18:15:14 +0200

    fix route

--- src/lib.rs
+++ src/lib.rs
@@ -13,7 +13,7 @@ pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
             .route("/health_check", web::get().to(health_check))
-            .route("subscribe", web::post().to(subscribe))
+            .route("/subscribe", web::post().to(subscribe))
     })
     .listen(listener)?
     .run();
