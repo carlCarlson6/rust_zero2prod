5570034d2cb0cc405b3312d24cdec973df1fee9c
carl <carlos.acitores@colba.es>
2024-04-05 15:11:11 +0200

    fix formatting

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use std::io::Error;
-use actix_web::{web::{self}, App, HttpRequest, HttpServer, Responder};
+use actix_web::{
+    web::{self}, 
+    App, HttpRequest, HttpServer, Responder
+};
 
 async fn greet(request: HttpRequest) -> impl Responder {
     let name = request.match_info().get("name").unwrap_or("Word");
@@ -11,8 +14,8 @@ async fn main() -> Result<(), Error> {
     println!("Hello world! - starting server");
     HttpServer::new(|| {
         App::new()
-        .route("/", web::get().to(greet))
-        .route("/{name}", web::get().to(greet))
+            .route("/", web::get().to(greet))
+            .route("/{name}", web::get().to(greet))
     })
     .bind("127.0.0.1:8000")?
     .run()
