0c9cdbaab7d499fd4975f51969f6d6ea2e50a2c1
carl <carlos.acitores@colba.es>
2024-04-07 14:47:32 +0200

    [refactor] actix service instead of route

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
-use actix_web::{dev::Server, web, App, HttpResponse, HttpServer};
+use actix_web::{dev::Server, get, post, web, App, HttpResponse, HttpServer};
 use std::net::TcpListener;
 
+#[get("/health_check")]
 async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
@@ -11,6 +12,7 @@ struct SubscribeFormData {
     name: String,
 }
 
+#[post("/subscriptions")]
 async fn subscribe(form: web::Form<SubscribeFormData>) -> HttpResponse {
     println!(
         "request recieved on subscriptions for {} - {}",
@@ -20,12 +22,8 @@ async fn subscribe(form: web::Form<SubscribeFormData>) -> HttpResponse {
 }
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
-        App::new()
-            .route("/health_check", web::get().to(health_check))
-            .route("/subscriptions", web::post().to(subscribe))
-    })
-    .listen(listener)?
-    .run();
+    let server = HttpServer::new(|| App::new().service(health_check).service(subscribe))
+        .listen(listener)?
+        .run();
     Ok(server)
 }
